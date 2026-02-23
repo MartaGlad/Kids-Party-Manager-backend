79b0c7cdb9930f1a24511552e6a3b31e343bc753
Marta Gladysz <gladysz.mrt@gmail.com>
2026-02-23 20:11:17 +0100

    Initial project setup

---
+++ src/main/java/com/gladysz/kidspartymanager/KidsPartyManagerApplication.java
@@ -0,0 +1,13 @@
+package com.gladysz.kidspartymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KidsPartyManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KidsPartyManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gladysz/kidspartymanager/KidsPartyManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gladysz.kidspartymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KidsPartyManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
