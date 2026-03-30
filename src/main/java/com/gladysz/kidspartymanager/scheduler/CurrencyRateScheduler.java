9bf34ffcd9ee91fe499c6b119b7a4e9dc88d8c6b
Marta Gladysz <gladysz.mrt@gmail.com>
2026-03-30 14:26:58 +0200

    Add logging for currency rate synchronization

--- src/main/java/com/gladysz/kidspartymanager/scheduler/CurrencyRateScheduler.java
+++ src/main/java/com/gladysz/kidspartymanager/scheduler/CurrencyRateScheduler.java
@@ -2,19 +2,34 @@ package com.gladysz.kidspartymanager.scheduler;
 
 import com.gladysz.kidspartymanager.service.CurrencyRateService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
 
 @Component
 @RequiredArgsConstructor
 public class CurrencyRateScheduler {
 
     private final CurrencyRateService currencyRateService;
+    private static final Logger LOGGER = LoggerFactory.getLogger(CurrencyRateScheduler.class);
 
     @Scheduled(cron = "0 0 13 * * MON-FRI")
     //@Scheduled(fixedRate = Long.MAX_VALUE, initialDelay = 0)
     public void updateCurrentRates() {
 
-        currencyRateService.fetchAndSaveActualRates();
+        LOGGER.info("Starting currency rates synchronization");
+
+        long startTime = System.currentTimeMillis();
+
+        try {
+            currencyRateService.fetchAndSaveActualRates();
+            LOGGER.info("Currency rates synchronization finished");
+        } catch (Exception e) {
+            LOGGER.error("Currency rates synchronization failed", e);
+        } finally {
+            LOGGER.info("Duration in ms = {}", System.currentTimeMillis() - startTime);
+        }
     }
 }
--- src/main/java/com/gladysz/kidspartymanager/service/integration/exchangerate/NbpExchangeRateAdapter.java
+++ src/main/java/com/gladysz/kidspartymanager/service/integration/exchangerate/NbpExchangeRateAdapter.java
@@ -6,6 +6,8 @@ import com.gladysz.kidspartymanager.dto.currencyrate.NbpRateDto;
 import com.gladysz.kidspartymanager.dto.currencyrate.NbpTableDto;
 import com.gladysz.kidspartymanager.exception.externalapi.ExternalApiException;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestClientException;
@@ -23,40 +25,46 @@ import java.util.stream.Collectors;
 public class NbpExchangeRateAdapter implements ExchangeRateProvider {
 
     private final RestClient restClient;
+    private final static Logger LOGGER = LoggerFactory.getLogger(NbpExchangeRateAdapter.class);
 
 
     @Override
     public CurrencyDataDto getCurrentCurrencyData() {
 
         try {
+            LOGGER.info("Sending request to NBP");
+
             NbpTableDto[] response = restClient.get()
                     .uri("https://api.nbp.pl/api/exchangerates/tables/A?format=json")
                     .retrieve()
                     .body(NbpTableDto[].class);
 
-        if (response == null || response.length == 0) {
-            throw new ExternalApiException("Empty response from NBP received.");
-        }
+            if (response == null || response.length == 0) {
+                throw new ExternalApiException("Empty response from NBP received.");
+            }
 
-        NbpTableDto tableA = response[0];
+            NbpTableDto tableA = response[0];
 
-        List<NbpRateDto> rates = tableA.rates();
-        LocalDate effectiveDate = tableA.effectiveDate();
+            List<NbpRateDto> rates = tableA.rates();
+            LocalDate effectiveDate = tableA.effectiveDate();
 
-        Set<String> requiredCodes = Set.of("EUR", "USD", "GBP");
+            LOGGER.info("Response from NBP received, effectiveDate = {}", effectiveDate);
 
-        Map<String, BigDecimal> filteredRatesMap = rates.stream()
+            Set<String> requiredCodes = Set.of("EUR", "USD", "GBP");
+
+            Map<String, BigDecimal> filteredRatesMap = rates.stream()
                     .filter(rate -> requiredCodes.contains(rate.code()))
                     .collect(Collectors.toMap(NbpRateDto::code, NbpRateDto::mid));
 
-        if (!filteredRatesMap.keySet().containsAll(requiredCodes)) {
-            throw new ExternalApiException("Missing required codes in NBP response.");
-        }
+            if (!filteredRatesMap.keySet().containsAll(requiredCodes)) {
+                throw new ExternalApiException("Missing required codes in NBP response.");
+            }
 
-        return new CurrencyDataDto(filteredRatesMap, effectiveDate);
+            return new CurrencyDataDto(filteredRatesMap, effectiveDate);
 
-        } catch (RestClientException e ) {
-            throw new ExternalApiException("Failed to fetch rates from NBP", e);
+        } catch (RestClientException e) {
+            LOGGER.error("Failed to fetch rates from NBP", e);
+            throw new ExternalApiException("Failed to fetch rates from NBP.", e);
         }
     }
 }
