889f1c28a4d5b5fa13f058c894d6f4ac7bb31f9b
Marta Gladysz <gladysz.mrt@gmail.com>
2026-04-01 15:25:20 +0200

    Add unit tests for updateReservation

--- src/test/java/com/gladysz/kidspartymanager/service/ReservationServiceTest.java
+++ src/test/java/com/gladysz/kidspartymanager/service/ReservationServiceTest.java
@@ -3,14 +3,11 @@ package com.gladysz.kidspartymanager.service;
 import com.gladysz.kidspartymanager.domain.*;
 import com.gladysz.kidspartymanager.domain.pricing.PricingResult;
 import com.gladysz.kidspartymanager.dto.reservation.ReservationCreateDto;
+import com.gladysz.kidspartymanager.dto.reservation.ReservationUpdateDto;
 import com.gladysz.kidspartymanager.exception.animator.AnimatorInactiveException;
-import com.gladysz.kidspartymanager.exception.reservation.ReservationFilteringException;
-import com.gladysz.kidspartymanager.exception.reservation.ReservationNotFoundException;
-import com.gladysz.kidspartymanager.exception.reservation.ReservationOverlapException;
-import com.gladysz.kidspartymanager.repository.EventPackageRepository;
+import com.gladysz.kidspartymanager.exception.reservation.*;
+import com.gladysz.kidspartymanager.mapper.ReservationMapper;
 import com.gladysz.kidspartymanager.repository.ReservationRepository;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -51,6 +48,9 @@ class ReservationServiceTest {
     @Mock
     private PricingService pricingService;
 
+
+    @Mock
+    private ReservationMapper reservationMapper;
 
 
 
@@ -333,6 +333,124 @@ class ReservationServiceTest {
         //When & Then
         assertThrows(ReservationNotFoundException.class,
                 () -> reservationService.changeReservationStatusById(1L, Status.COMPLETED));
+    }
+
+
+    @Test
+    void shouldUpdateReservationSuccessfully() {
+
+        //Given
+        Reservation r1 = new Reservation();
+        r1.setEventDateTime(LocalDateTime.of(2026, 1, 20, 12, 0,0));
+        r1.setStatus(Status.NEW);
+        r1.setBirthdayChildAge(1);
+
+        ReservationUpdateDto dto = new ReservationUpdateDto(
+                1L, LocalDateTime.of(2026, 1, 10, 12, 0,0),
+                16, 3);
+
+        EventPackage eventPackage = new EventPackage();
+        eventPackage.setDurationHr(2);
+
+        PricingResult pricingResult = new PricingResult(BigDecimal.valueOf(1000), BigDecimal.valueOf(233.06),
+                BigDecimal.valueOf(269.01), BigDecimal.valueOf(203.41), false);
+
+        when(reservationRepository.findById(1L)).thenReturn(Optional.of(r1));
+        when(eventPackageService.getEventPackageById(1L)).thenReturn(eventPackage);
+
+        doAnswer(i -> {
+            Reservation reservation = i.getArgument(0);
+            ReservationUpdateDto updateDto = i.getArgument(1);
+
+            reservation.setEventDateTime(updateDto.eventDateTime());
+            reservation.setChildrenCount(updateDto.childrenCount());
+            reservation.setBirthdayChildAge(updateDto.birthdayChildAge());
+
+            return null;
+        }).when(reservationMapper).applyUpdate(r1, dto);
+
+        when(pricingService.getPricingResult(eventPackage, dto.childrenCount(), dto.eventDateTime().toLocalDate()))
+                .thenReturn(pricingResult);
+
+        //When
+        reservationService.updateReservation(1L, dto);
+
+        //Then
+        assertEquals(dto.eventDateTime(), r1.getEventDateTime());
+        assertEquals(pricingResult.finalPricePln(), r1.getPriceSnapshot());
+        assertEquals(3, r1.getBirthdayChildAge());
+        verify(reservationMapper).applyUpdate(r1, dto);
+        verify(pricingService).getPricingResult(eventPackage, dto.childrenCount(), dto.eventDateTime().toLocalDate());
+    }
+
+
+    @Test
+    void shouldThrowExceptionWhenReservationCannotBeUpdated() {
+        //Given
+        Reservation r1 = new Reservation();
+        r1.setStatus(Status.COMPLETED);
+
+        ReservationUpdateDto dto = new ReservationUpdateDto(
+                1L, LocalDateTime.of(2026, 1, 10, 12, 0,0),
+                16, 3);
+
+        when(reservationRepository.findById(1L)).thenReturn(Optional.of(r1));
+
+        //When & Then
+        assertThrows(ReservationUpdateException.class, () ->
+                reservationService.updateReservation(1L, dto));
+        verify(reservationRepository).findById(1L);
+    }
+
+
+    @Test
+    void shouldThrowExceptionWhenUpdatedReservationOverlaps() {
+        //Given
+        ReservationUpdateDto dto = new ReservationUpdateDto(
+                1L, LocalDateTime.of(2026, 1, 10, 12, 0,0),
+                16, 3);
+
+        EventPackage eventPackage = new EventPackage();
+        eventPackage.setDurationHr(2);
+
+        Reservation rUpdated = new Reservation();
+        ReflectionTestUtils.setField(rUpdated, "id", 1L);
+        rUpdated.setStatus(Status.NEW);
+        rUpdated.setEventDateTime(LocalDateTime.of(2026, 1, 9, 12, 0,0));
+        rUpdated.setEventPackage(eventPackage);
+
+        Reservation rCollided = new Reservation();
+        ReflectionTestUtils.setField(rCollided, "id", 5L);
+        rCollided.setStatus(Status.NEW);
+        rCollided.setEventDateTime(LocalDateTime.of(2026, 1, 10, 11, 0,0));
+        rCollided.setEventPackage(eventPackage);
+
+        LocalDateTime dayStart = LocalDate.of(2026, 1, 10).atStartOfDay();
+        LocalDateTime nextDayStart = LocalDate.of(2026, 1, 10).plusDays(1).atStartOfDay();
+
+        when(reservationRepository.findById(1L)).thenReturn(Optional.of(rUpdated));
+        when(eventPackageService.getEventPackageById(1L)).thenReturn(eventPackage);
+        when(reservationRepository.findActiveReservationsForDay(
+                dayStart, nextDayStart)).thenReturn(List.of(rCollided));
+
+        //When & Then
+        assertThrows(ReservationOverlapException.class, () -> reservationService.updateReservation(1L, dto));
+    }
+
+
+    @Test
+    void shouldThrowExceptionWhenUpdatedReservationNotFound() {
+
+        //Given
+        ReservationUpdateDto dto = new ReservationUpdateDto(
+                1L, LocalDateTime.of(2026, 1, 10, 12, 0,0),
+                16, 3);
+        when(reservationRepository.findById(1L)).thenReturn(Optional.empty());
+
+        //When & Then
+        assertThrows(ReservationNotFoundException.class, () -> reservationService.updateReservation(1L, dto));
+        verify(reservationRepository).findById(1L);
 
     }
+
 }
